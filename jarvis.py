7469207635daf784f25019b94eaf147b4d6bace7
urmil89 <urmil8989@gmail.com>
2021-08-13 08:02:20 +0530

    jarvis is on

--- jarvis.py
+++ jarvis.py
@@ -1,10 +1,11 @@
-import pyttsx3
 import datetime
+import os
+import webbrowser
 from datetime import datetime
+
+import pyttsx3
 import speech_recognition as sr
 import wikipedia
-import webbrowser
-import os
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -30,10 +31,14 @@ def wishMe():
 
 def takeCommand():
     r = sr.Recognizer()
+    # with mic as source:
     with sr.Microphone() as source:
         print("Listening...")
+        r.energy_threshold = 100
         r.pause_threshold = 1
+        speak("Listening...")
         audio = r.listen(source)
+        print("Listening...")
 
     try:
         print("Recognizing...")
@@ -43,15 +48,25 @@ def takeCommand():
     except Exception as e:
         # print(e)
         print("say that again please...")
+        speak("say that again please...")
         return "None"
     return query
 
 
 if __name__ == "__main__":
-    wishMe()
+    # wishMe()
     while True:
+
         query = takeCommand().lower()
-        if 'wikipedia' in query:
+
+        if 'hey jarvis' in query:
+            speak("hi, i am jarvis, i can do thing like")
+            task_list = ['search on wikipedia', 'open your sites', 'current time', 'open softwares',
+                         'and many more functionalities']
+            for item in task_list:
+                speak(item)
+
+        elif 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentences=3)
@@ -75,7 +90,7 @@ if __name__ == "__main__":
             webbrowser.get(chrome).open("stackoverflow.com")
 
         elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            strTime = datetime.now().strftime("%H:%M:%S")
             print(strTime)
             speak(f"Sir,the time is {strTime}")
 
@@ -83,13 +98,9 @@ if __name__ == "__main__":
             codePath = "C:\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
-        elif 'who are you' in query and 'tell me about yourself' in query:
+        elif 'who are you' in query or 'tell me about yourself' in query:
             speak("I am jarvis and i'm urmilsir assistant i am made by Urmil")
 
         elif 'by by jarvis' in query:
             speak("thanks for your time")
-            break
-
-        else:
-            speak("something fault in me")
             break
